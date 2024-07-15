a39e55453d83e45f9aad1c97cdb96b651e94e941
Sam MBP <snevarez89@gmail.com>
2024-07-15 14:22:57 -0700

    finished and migrated to go-daddy

--- contact.php
+++ contact.php
@@ -1,28 +1,40 @@
 <?php 
 
-    if(isset($_POST['btn-send']))
+if(isset($_POST['btn-send']))
+{
+    $UserName = $_POST['UName'];
+    $Email = $_POST['Email'];
+    $Msg = $_POST['msg'];
+
+    if(empty($UserName) || empty($Email) || empty($Msg))
     {
-       $UserName = $_POST['UName'];
-       $Email = $_POST['Email'];
-       $Subject = $_POST['Subject'];
-       $Msg = $_POST['msg'];
-
-       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
-       {
-           header('location:index.php?error#form-message-loction');
-       }
-       else
-       {
-           $to = "qstacos@gmail.com";
-
-           if(mail($to,$Subject,$Msg,$Email))
-           {
-               header("location:index.php?success#form-message-loction");
-           }
-       }
+        header('location:index.php?error#form-message-location');
     }
     else
     {
-        header("location:index.php");
+        $to = "adaptify.design@gmail.com";
+        $bcc = "snevarez89@gmail.com";
+        $subject = "Q's Tacos - New Form Submission Received!";
+        $headers = "From: noreply@qstacos.com\r\n";
+        $headers .= "Reply-To: $Email\r\n";
+        $headers .= "BCC: $bcc\r\n";
+        
+        $message = "Name: $UserName\r\n";
+        $message .= "Email: $Email\r\n";
+        $message .= "Message:\r\n$Msg\r\n";
+
+        if(mail($to, $subject, $message, $headers))
+        {
+            header("location:index.php?success#form-message-location");
+        }
+        else
+        {
+            header("location:index.php?error#form-message-location");
+        }
     }
+}
+else
+{
+    header("location:index.php");
+}
 ?>
